1778f3c592d4988d6d2b23c3b6afc3fbf36bed05
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2014-12-15 10:56:40 +0100

    more sane return values for REST
    [#84279964]

--- lib/devise-ios-rails.rb
+++ lib/devise-ios-rails.rb
@@ -30,6 +30,17 @@ module DeviseIosRails
             resource.errors
           end
 
+          # simply render the resource even on POST instead of redirecting for ajax
+          def api_behavior(error)
+            if post?
+              display resource, :status => :created
+            # render resource instead of 204 no content
+            elsif put? || delete?
+              display resource, :status => :ok
+            else
+              head :no_content
+            end
+          end
         end
       end
     end
