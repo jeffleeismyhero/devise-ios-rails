c49ef354928fc78cc9e8c00c34f0e4190e97e863
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2015-02-18 19:33:58 +0100

    Schema update

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -29,20 +29,20 @@ ActiveRecord::Schema.define(version: 20141215153026) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
   create_table "secret_spaces", force: true do |t|
-    t.string   "text",       limit: nil
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  limit: nil, default: "", null: false
-    t.string   "encrypted_password",     limit: nil, default: "", null: false
-    t.string   "reset_password_token",   limit: nil
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "authentication_token",   limit: nil
+    t.string   "authentication_token"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
